b2836c198f58e2a82dc8472a3de7616643c9891f
Vadim-Akmeev <akmeevv@gmail.com>
2023-05-23 12:47:14 +0300

    declaration camelCase

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,10 +4,31 @@ add comment*/
 
 var a; //creating a variable
 
-a = 5; //assigning a value
+a = 5; //assigning a value number
 
 var b; //creating a variable
 
 b = a; //assigning a variable value another variable
 
 var a = 9; //creating a variable and assigning a value in one command
+
+var myFirstName = 'Vadim';// Diclare string variables
+
+var myLastName = 'Akmeev';// Declare string variables
+
+a = 5; // обьединение результатов переменных
+b = 10;
+c = 'I am a';
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations camelCase
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments camelCase
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
