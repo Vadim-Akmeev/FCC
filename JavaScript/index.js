f9160289824315e99dba38ce1b7b9bfa5204903f
Vadim-Akmeev <akmeevv@gmail.com>
2023-05-23 12:15:58 +0300

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,10 +2,12 @@
 /*this is 
 add comment*/
 
-var a;
+var a; //creating a variable
 
-a = 5;
+a = 5; //assigning a value
 
-var b;
+var b; //creating a variable
 
-b = a;
+b = a; //assigning a variable value another variable
+
+var a = 9; //creating a variable and assigning a value in one command
