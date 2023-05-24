c75ed6988269bee096bdf7bd081626c4623ded53
Vadim-Akmeev <akmeevv@gmail.com>
2023-05-24 16:51:28 +0300

    сложение с присваиванием

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -81,3 +81,13 @@ const quotient = 4.4 / 2.0; // Change this line
 
 //Поиск остатка
 const remainder = 11 % 3;
+
+//сложение с присваиванием
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a +=12;
+b += 9 ;
+c += 7;
